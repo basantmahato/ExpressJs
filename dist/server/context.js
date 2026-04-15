a90a88b4ff1e9a1924a469be5103edee0aa972f3
basant <basantkumar72580@gmail.com>
2026-04-15 16:39:26 +0530

    trpc

--- dist/server/context.js
+++
@@ -1,5 +0,0 @@
-import * as trpcExpress from "@trpc/server/adapters/express";
-export const createContext = ({ req, res, }) => {
-    return { req, res };
-};
-//# sourceMappingURL=context.js.map
--- dist/server/trpc.js
+++
@@ -1,6 +0,0 @@
-import { initTRPC } from "@trpc/server";
-// Create a TRPC context
-const trpc = initTRPC.context().meta().create();
-export const router = trpc.router;
-export const publicProcedure = trpc.procedure;
-//# sourceMappingURL=trpc.js.map
